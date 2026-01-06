a77b44ab1ba19960dd7fa2bb0fab6ae9c4acfdd3
vinayagam <vinayak.inode@gmail.com>
2026-01-06 18:31:40 +0530

    Revert "github optimised"
    This reverts commit 63734cc5b97b7dc10ec604178b3eaff0863e6e76.

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  output: 'export',
-  distDir: 'docs',
+  /* config options here */
   reactCompiler: true,
-  basePath: isProd ? '/pos' : '',
-  assetPrefix: isProd ? '/pos/' : '',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
